7ce8c77399aa562438c98cac78fa1b44f1561e1c
Deepak <maskman.deepak911@gmail.com>
2024-03-02 12:10:03 +0530

    commit pluginReWriteAll

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),pluginRewriteAll()],
+
+  plugins: [react(),
+    // pluginRewriteAll()
+  ],
 })
+
+
+// export default defineConfig(({ command, mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '');
+//   return {
+//       define: {
+//           'process.env.API_BASE': JSON.stringify(env.API_BASE),
+//           'process.env.YOUR_BOOLEAN_VARIABLE': env.YOUR_BOOLEAN_VARIABLE,
+//           // If you want to exposes all env variables, which is not recommended
+//           // 'process.env': env
+//       },
+//       plugins: [react(),pluginRewriteAll()],
+//   };
+// });
